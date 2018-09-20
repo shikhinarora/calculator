7037a34b369ce65697aad8457d02298067b62252
shikhin <shikhin.arora@gmail.com>
2018-09-20 22:59:08 +0530

    final changes

--- app/src/main/java/com/technard/waycalc/ui/ActivityCalc.java
+++ app/src/main/java/com/technard/waycalc/ui/ActivityCalc.java
@@ -2,11 +2,13 @@ package com.technard.waycalc.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.technard.waycalc.R;
@@ -16,12 +18,9 @@ import java.util.ArrayList;
 public class ActivityCalc extends AppCompatActivity implements View.OnClickListener{
 
     private EditText et;
-    private TextView tvAdd, tvSub, tvEqual, tvDecimal, tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8, tv9, tv0;
-
-    private ArrayList<Integer> nos = new ArrayList<>();
-    private ArrayList<String> operations = new ArrayList<>();
-    private int answer;
-
+    private TextView tvAdd, tvSub, tvMul, tvDivide, tvEqual, tvDecimal, tvDel, tv1, tv2, tv3, tv4,
+            tv5, tv6, tv7, tv8, tv9, tv0;
+    private LinearLayout llMain;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,13 +30,14 @@ public class ActivityCalc extends AppCompatActivity implements View.OnClickListe
     }
 
     private void init() {
-        answer = 0;
-
         et = findViewById(R.id.et_calc);
         tvAdd = findViewById(R.id.b_add);
         tvSub = findViewById(R.id.b_sub);
+        tvMul = findViewById(R.id.b_mul);
+        tvDivide = findViewById(R.id.b_divide);
         tvEqual = findViewById(R.id.b_equal);
         tvDecimal = findViewById(R.id.b_decimal);
+        tvDel = findViewById(R.id.b_del);
         tv1 = findViewById(R.id.b_1);
         tv2 = findViewById(R.id.b_2);
         tv3 = findViewById(R.id.b_3);
@@ -48,6 +48,7 @@ public class ActivityCalc extends AppCompatActivity implements View.OnClickListe
         tv8 = findViewById(R.id.b_8);
         tv9 = findViewById(R.id.b_9);
         tv0 = findViewById(R.id.b_0);
+        llMain = findViewById(R.id.ll_main);
 
         tv0.setOnClickListener(this);
         tv1.setOnClickListener(this);
@@ -61,6 +62,10 @@ public class ActivityCalc extends AppCompatActivity implements View.OnClickListe
         tv9.setOnClickListener(this);
         tvAdd.setOnClickListener(this);
         tvSub.setOnClickListener(this);
+        tvMul.setOnClickListener(this);
+        tvDivide.setOnClickListener(this);
+        tvDecimal.setOnClickListener(this);
+        tvDel.setOnClickListener(this);
         tvEqual.setOnClickListener(this);
 
     }
@@ -104,71 +109,173 @@ public class ActivityCalc extends AppCompatActivity implements View.OnClickListe
             case R.id.b_sub:
                 insertOperation("-");
                 break;
+            case R.id.b_mul:
+                insertOperation("*");
+                break;
+            case R.id.b_divide:
+                insertOperation("/");
+                break;
+            case R.id.b_decimal:
+                insertDecimal();
+                break;
             case R.id.b_equal:
                 calculate();
+                break;
+            case R.id.b_del:
+                delete();
+                break;
+            default:
                 break;
         }
     }
 
-    private void calculate() {
+    private void delete() {
         String curr = et.getText().toString();
+        int currLen = curr.length();
 
-        if(!curr.equals("+") || !curr.equals("-")){
-            nos.add(Integer.valueOf(curr));
+        if (currLen <= 1){
+            et.setText("0");
+        } else {
+            et.setText(curr.substring(0, currLen-1));
+        }
+    }
+
+    private void calculate(){
+        String expression = et.getText().toString();
+        int len = expression.length();
+
+        if(len > 0){
+            String firstChar = expression.substring(0, 1);
+            if (isOperator(firstChar)){
+                if(firstChar.equals("-")){
+                    expression = "0" + expression;
+                    len++;
+                } else {
+                    expression = expression.substring(1, len);
+                    len--;
+                }
+            }
+
+            String nOne = "", nTwo = "";
+            String operator = "";
+
+            for(int i = 0; i < len; i++){
+                String chr = String.valueOf(expression.charAt(i));
+
+                if (isOperator(chr)){
+                    if (operator.equals(""))
+                        operator = chr;
+                    else{
+                        double n1 = Double.parseDouble(nOne);
+                        double n2 = Double.parseDouble(nTwo);
+
+                        nOne = String.valueOf(useOperator(n1, n2, operator));
+                        nTwo = "";
+                        operator = chr;
+                    }
+                } else {
+                    if(nOne.equals(""))
+                        nOne = nOne + chr;
+                    else{
+                        if (operator.equals(""))
+                            nOne = nOne + chr;
+                        else
+                            nTwo = nTwo + chr;
+                    }
+                }
+
+                if (i == len - 1){
+                    if(!nTwo.equals("") && !operator.equals("")){
+                        double n1 = Double.parseDouble(nOne);
+                        double n2 = Double.parseDouble(nTwo);
+
+                        nOne = String.valueOf(useOperator(n1, n2, operator));
+                    }
+                }
+            }
+
+            if (nOne.equals(""))
+                nOne = "0";
+
+            String[] tempAns = nOne.split("\\.");
+
+            if(tempAns.length > 1){
+                int dec = Integer.parseInt(tempAns[1]);
+                if(dec != 0 || expression.contains(".")) {
+                    double answer = Double.parseDouble(nOne);
+                    et.setText("" + answer);
+                } else{
+                    nOne = tempAns[0];
+                    int answer = Integer.parseInt(nOne);
+                    et.setText("" + answer);
+                }
+            } else {
+                int answer = Integer.parseInt(nOne);
+                et.setText("" + answer);
+            }
+        } else {
+            et.setText("0");
+        }
+    }
+
+    private boolean isOperator(String chr){
+        if (chr.equals("+") || chr.equals("-") || chr.equals("*") || chr.equals("/"))
+            return true;
+        else
+            return false;
+    }
+
+    private double useOperator(double one, double two, String operator){
+        double answer = 0;
+
+        if(operator.equals("+"))
+            answer = one + two;
+
+        if(operator.equals("-"))
+            answer = one - two;
+
+        if(operator.equals("*"))
+            answer = one * two;
+
+        if(operator.equals("/")){
+            if(two == 0){
+                Snackbar.make(llMain, "Canot divide by zero", Snackbar.LENGTH_SHORT).show();
+            } else
+                answer = one / two;
         }
 
-        int sizeN = nos.size();
-        int sizeO = operations.size();
-        answer = nos.get(sizeN - 1);
-        nos.remove(sizeN - 1);
-
-        while(nos.size() > 0){
-            sizeN = nos.size();
-            sizeO = operations.size();
-
-            int number = nos.get(sizeN - 1);
-            String operation = operations.get(sizeO - 1);
-
-            if(operation.equals("+"))
-                answer += number;
-
-            if(operation.equals("-"))
-                answer -= (number * -1);
-
-            nos.remove(sizeN - 1);
-            operations.remove(sizeO - 1);
-
-            Log.d("Calc", "calculate: " + answer);
-        }
-
-        et.setText("" + answer);
+        return answer;
     }
 
     private void insertNumber(int digit){
-        int curr = Integer.parseInt(et.getText().toString());
+        String curr = et.getText().toString();
 
-        if(curr == 0){
+        if(curr.equals("0")){
             et.setText("" + digit);
         } else {
             et.setText("" + curr + digit);
         }
-
-
     }
 
     private void insertOperation(String operation){
-        int curr = Integer.parseInt(et.getText().toString());
+        String curr = et.getText().toString();
+        int currLen = curr.length();
 
-        et.setText("" + curr + operation);
-
-        nos.add(curr);
-        operations.add(operation);
-
-        et.setText("0");
+        if(currLen == 1 && isOperator(curr))
+            et.setText(curr + operation);
+        else{
+            if (isOperator(curr.substring(currLen-1, currLen)))
+                et.setText(curr.substring(0, currLen-1) + operation);
+            else
+                et.setText(curr + operation);
+        }
     }
 
-    private String getLastCharacter(){
+    private void insertDecimal(){
         String curr = et.getText().toString();
-        return curr.substring(curr.length()-2, curr.length()-1);
+        int currLen = curr.length();
+
+        if(!curr.substring(currLen-1, currLen).equals(".") && !isOperator(curr.substring(currLen-1, currLen)))
+            et.setText(curr + ".");
     }
 }
